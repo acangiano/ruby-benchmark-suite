a4ea46ab365317f06c0c86f23a6ef9037d9a5823
rogerdpack <rogerpack2005@gmail.com>
2009-03-06 14:14:16 -0700

    add in the other bm_vm1_const.rb

--- core-features/bm_vm1_const2.rb
+++ core-features/bm_vm1_const2.rb
@@ -1,3 +1,4 @@
+# from ruby 1.9 benchmarks, slightly modified.
 require File.dirname(__FILE__) + '/../lib/benchutils'
 
 label = File.expand_path(__FILE__).sub(File.expand_path("..") + "/", "")
@@ -5,17 +6,17 @@ iterations = ARGV[-3].to_i
 timeout = ARGV[-2].to_i
 report = ARGV.last
 
-n = 500_000
-iter = 0
-
 benchmark = BenchmarkRunner.new(label, iterations, timeout)
 benchmark.run do
-  iter +=1 
-  puts iter
-  n.times do |i|
-    Object.const_set("CONST#{iter}#{i}", 1)
-    const = Object.const_get("CONST#{iter}#{i}")
-  end
+
+Const = 1 
+i = 0 
+while i<3000000
+  i+= Const 
+  j = Const 
+  k = Const 
+end 
+
 end
 
 File.open(report, "a") {|f| f.puts "#{benchmark.to_s},n/a" }
