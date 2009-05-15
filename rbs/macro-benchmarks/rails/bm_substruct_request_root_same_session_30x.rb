68b238037fb6ef25fd29aa3c1df4cf1a16572933
rogerdpack <rogerpack2005@gmail.com>
2009-05-15 15:14:04 -0600

    remove unnecessary requires

--- rbs/macro-benchmarks/rails/bm_substruct_request_root_same_session_30x.rb
+++ rbs/macro-benchmarks/rails/bm_substruct_request_root_same_session_30x.rb
@@ -1,7 +1,4 @@
 require 'substruct_start_and_bootstrap_if_necessary.rb'
-require 'config/environment'
-require 'application_controller'
-require 'action_controller/request_profiler'
 
 ActionController::RequestProfiler.run(%w[-b -n1 request_root]) # warmup
 
