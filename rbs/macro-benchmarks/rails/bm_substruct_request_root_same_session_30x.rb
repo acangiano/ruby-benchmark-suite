919acb13bc9f0edb877e03c1055173dcb896fe8f
rogerdpack <rogerpack2005@gmail.com>
2009-05-15 15:03:51 -0600

    make rails tests slightly quicker

--- rbs/macro-benchmarks/rails/bm_substruct_request_root_same_session_30x.rb
+++ rbs/macro-benchmarks/rails/bm_substruct_request_root_same_session_30x.rb
@@ -6,5 +6,5 @@ require 'action_controller/request_profiler'
 ActionController::RequestProfiler.run(%w[-b -n1 request_root]) # warmup
 
 Bench.run [100] do
-    ActionController::RequestProfiler.run(%w[-b -n100 request_root])
+    ActionController::RequestProfiler.run(%w[-b -n1 request_root_15x]) # runs it 2x15 times, one for warmup
 end
