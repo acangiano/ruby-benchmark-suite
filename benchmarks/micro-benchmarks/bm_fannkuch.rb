2276f994f858b527942bd7a6e2078c94530b63bd
Antonio Cangiano <acangiano@gmail.com>
2010-06-24 15:00:46 -0400

    Update to newest version available on the CLBG.

--- benchmarks/micro-benchmarks/bm_fannkuch.rb
+++ benchmarks/micro-benchmarks/bm_fannkuch.rb
@@ -1,42 +1,65 @@
-# The Computer Language Shootout
+# From The Computer Language Benchmarks Game
 # http://shootout.alioth.debian.org/
-# Contributed by Sokolov Yura
-# Modified by Ryan Williams
+# Contributed by Wesley Moxam
 
 def fannkuch(n)
-   maxFlips, m, r, check = 0, n-1, n, 0
-   count = (1..n).to_a
-   perm = (1..n).to_a
+  sign, maxflips, sum = 1, 0, 0
 
-   while true
-      if check < 30
-         #puts "#{perm}"
-         check += 1
-      end
+  p = [nil].concat((1..n).to_a)
+  q = p.dup
+  s = p.dup
 
-      while r != 1
-         count[r-1] = r
-         r -= 1
+  while(true)
+    # Copy and flip.
+    q1 = p[1]				# Cache 1st element.
+    if q1 != 1
+      q = p.dup
+      flips = 1
+      while(true)
+	      qq = q[q1]
+	      if qq == 1				# ... until 1st element is 1.
+	        sum = sum + sign * flips
+	        maxflips = flips if flips > maxflips # New maximum?
+	        break
+	      end
+	      q[q1] = q1
+	      if q1 >= 4
+	        i, j = 2, q1 - 1
+	        begin
+            q[i], q[j] = q[j], q[i]
+            i = i + 1
+            j = j - 1
+          end while i < j
+	      end
+	      q1 = qq
+        flips = flips + 1
       end
-
-      if perm[0] != 1 and perm[m] != n
-         perml = perm.clone #.dup
-         flips = 0
-         while (k = perml.first ) != 1
-            perml = perml.slice!(0, k).reverse + perml
-            flips += 1
-         end
-         maxFlips = flips if flips > maxFlips
+    end
+    # Permute.
+    if sign == 1
+      # Rotate 1<-2.
+      p[1], p[2] = p[2], p[1]
+      sign = -1
+    else
+      # Rotate 1<-2 and 1<-2<-3.
+      p[2], p[3] = p[3], p[2]
+      sign = 1
+      3.upto(n) do |i|
+        (s[i] =  s[i] - 1) && break unless s[i] == 1
+	      return [sum, maxflips] if i == n 	# Out of permutations.
+	      s[i] = i
+        # Rotate 1<-...<-i+1.
+	      t = p[1]
+        1.upto(i) do |j|
+          p[j] = p[j+1]
+        end
+        p[i+1] = t
       end
-      while true
-         return maxFlips if r == n
-         perm.insert r,perm.shift
-         break if (count[r] -= 1) > 0
-         r += 1
-      end
-   end
+    end
+  end
 end
 
 Bench.run [6, 8, 10] do |n|
-  puts "Pfannkuchen(#{n}) = #{fannkuch(n)}"
+  sum, flips = fannkuch(n)
+  printf "%d\nPfannkuchen(%d) = %d\n", sum, n, flips
 end
